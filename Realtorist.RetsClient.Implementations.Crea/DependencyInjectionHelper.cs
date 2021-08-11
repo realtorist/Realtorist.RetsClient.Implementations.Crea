fe629fc84d64e80e2e654a1b871da39084441f01
Dmytro Bondarchuk <dmytro.bondarchuk@hootsuite.com>
2021-08-11 13:48:37 -0400

    Initial commit

---
+++ Realtorist.RetsClient.Implementations.Crea/DependencyInjectionHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Realtorist.RetsClient.Implementations.Crea.Abstractions;
+using Realtorist.RetsClient.Implementations.Crea.Implementations;
+
+namespace Realtorist.RetsClient.Implementations.Crea
+{
+    /// <summary>
+    /// Provides dependency injection helper methods
+    /// </summary>
+    public static class DependencyInjectionHelper
+    {
+        /// <summary>
+        /// Adds Automapper profile for CREA
+        /// </summary>
+        /// <param name="services">Services collection</param>
+        public static void AddCreaAutoMapper(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new CreaAutoMapperProfile());
+            });
+
+            mapperConfig.AssertConfigurationIsValid();
+            services.AddAutoMapper(typeof(CreaAutoMapperProfile));
+        }
+        
+        /// <summary>
+        /// Configures services related to CREA RETS Client
+        /// </summary>
+        /// <param name="services">Services collection</param>
+        public static void ConfigureCreaServices(this IServiceCollection services)
+        {
+            services.AddHttpClient<IDdfClient, DdfClient>()
+                .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(600)));
+        }
+    }
+}
